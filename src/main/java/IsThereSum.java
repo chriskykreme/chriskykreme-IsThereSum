8d1d14f67688ed0a155b0660df0db4e7eaedce41
Christopher King <king.cdk@gmail.com>
2023-08-08 03:53:33 +0000

    added logic to check

--- src/main/java/IsThereSum.java
+++ src/main/java/IsThereSum.java
@@ -10,6 +10,17 @@ public class IsThereSum {
      * @return true if two separate values in the array add up to a target, false otherwise.
      */
     public boolean check(int[] arr, int target){
+        for (int ctr1 = 0; ctr1 < arr.length; ctr1++) {
+            for (int ctr2 = 0; ctr2 < arr.length; ctr2++) {
+                if (ctr2 == ctr1) {
+                    continue;
+                }
+
+                if (arr[ctr1] + arr[ctr2] == target) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
